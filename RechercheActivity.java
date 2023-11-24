fc6460c149d56e078eb16d36e72cb465eca56da0
BrasSelva <147380508+BrasSelva@users.noreply.github.com>
2023-11-24 10:58:46 +0100

    Add files via upload

---
+++ RechercheActivity.java
@@ -0,0 +1,14 @@
+package com.example.weather;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RechercheActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_recherche);
+    }
+}
+
+
